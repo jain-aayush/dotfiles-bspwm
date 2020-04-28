e362ebcbf3eb9aa59877ca0ad00880e3a301e2f4
Aayush Jain <patni.aayush1998@gmail.com>
2020-04-28 22:51:01 +0530

    Changed the launch script for new polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar workspaces -c ~/.config/polybar/config.ini &
-polybar info -c ~/.config/polybar/config.ini &
+# Launch bar
+polybar example -c .config/polybar/config.ini 
